847d8dd9385a1ac8e03a77a6ac09dcd8846fcc40
Fahadfrj <112732586+Fahadfrj@users.noreply.github.com>
2022-10-27 13:39:20 +0200

    ferdig

--- src/Oblig3/oppgave1.java
+++ src/Oblig3/oppgave1.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 
  class UnikeTall{
-    public int[] list;
+    private int[] list;
    public UnikeTall(int l) {
        list = new int[l];
    }
@@ -13,14 +13,13 @@ import java.util.*;
             if(x == list[i]){
                 return false;
             }
-
         }
         return true;
      }
      public void fyll(){
          int x;
          for(int i=0; i< list.length; i++){
-             x = (int)(Math.random()*11);
+             x = (int)(Math.random()*(900)+100);
              if(sjekk(x)){
                  list[i] = x;
              }
@@ -31,16 +30,14 @@ import java.util.*;
      }
 
    public int max(){
-       int y = list.length;
        Arrays.sort(list);
-       return list[y-1];
+       return list[list.length - 1];
    }
    public int min(){
        Arrays.sort(list);
        return list[0];
    }
    public double gjennomsnitt(){
-
         double sum =0;
         for (int i=0; i<list.length; i++){
             sum+= list[i];
@@ -55,8 +52,7 @@ import java.util.*;
            }
            s += list[i] + " ";
        }
-    showMessageDialog(null,  s + "\n minste tallet er " + min() + "\n største tallet er " + max() + "\n gjennomsnittet er "+ gjennomsnitt() );
-
+    showMessageDialog(null,  s + "\n minste tallet er " + min() + "\n største tallet er " + max() + "\n gjennomsnittet er "+ String.format("%.2f",gjennomsnitt()) );
    }
 
 }
@@ -67,21 +63,18 @@ public class oppgave1 {
         while(true){
             try{
                 l = Integer.parseInt(showInputDialog("skriv inn arrays lengde"));
-                if(l<0){
+                if(l<=0){
                     showMessageDialog(null,"tallet må være større enn 0");
                     continue;
                 }
                 break;
             }catch (Exception e){
                showMessageDialog(null, "skriv inn gyldig nummer");
-
             }
         }
-
     UnikeTall tall = new UnikeTall(l);
     tall.fyll();
-
-  tall.finish();
+    tall.finish();
 
     }
 
